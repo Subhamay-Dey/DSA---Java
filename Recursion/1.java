bc56be6b5237114292f576a6471c0096d95b1c6d
Subhamay-Dey <ncdey1966@gmail.com>
2024-05-19 23:40:13 +0530

    Print Numbers from 5 to 1 through recusion and not by iteration.

---
+++ Recursion/1.java
@@ -0,0 +1,14 @@
+public class Recursion1 {
+    public static void printNumb(int n) {
+        if(n == 0) {
+            return;
+        }
+        System.out.println(n);
+        printNumb(n-1);
+    }
+
+    public static void main(String args[]) {
+        int n = 6;
+        printNumb(n);
+    }
+}
