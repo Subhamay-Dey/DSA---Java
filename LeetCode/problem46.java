5c4f4814fbaac1100236e421a5fc887fd1e2f5b6
Subhamay-Dey <ncdey1966@gmail.com>
2024-05-23 04:41:34 +0530

    Next Greater Element I -- solved through stack (leetcode).

---
+++ LeetCode/problem46.java
@@ -0,0 +1,26 @@
+package LeetCode;
+
+import java.util.HashMap;
+import java.util.Stack;
+
+public class problem46 {
+    class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        @SuppressWarnings({ "rawtypes", "unchecked" })
+        HashMap<Integer, Integer> next_greatest = new HashMap();
+        Stack<Integer> stack = new Stack<Integer>();
+
+        for(Integer num : nums2) {
+            while(!stack.isEmpty() && stack.peek() < num) {
+                next_greatest.put(stack.pop(), num);
+            }
+            stack.push(num);
+        }
+
+        for(int i = 0; i < nums1.length; i++) {
+            nums1[i] = next_greatest.getOrDefault(nums1[i], -1) ;
+        }
+        return nums1;
+    }
+}
+}
