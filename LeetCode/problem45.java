219445a6b5f3855e21d7f6d62432957e9476def8
Subhamay-Dey <ncdey1966@gmail.com>
2024-05-23 04:21:05 +0530

    Valid Parentheses -- solved through stack (leetcode).

---
+++ LeetCode/problem45.java
@@ -0,0 +1,28 @@
+package LeetCode;
+
+import java.util.Stack;
+
+public class problem45 {
+    class Solution {
+    public boolean isValid(String s) {
+        if(s.length() % 2 != 0) return false;
+
+        Stack<Character> stack = new Stack<Character>();
+        for(char c : s.toCharArray()) {
+            if(c == '(' || c == '{' || c == '['){
+                stack.push(c);
+            } else if(c == ')' && !stack.isEmpty() && stack.peek() == '(') {
+                stack.pop();
+            } else if(c == '}' && !stack.isEmpty() && stack.peek() == '{') {
+                stack.pop();
+            } else if(c == ']' && !stack.isEmpty() && stack.peek() == '[') {
+                stack.pop();
+            }
+            else{
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
+}
