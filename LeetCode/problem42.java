59b1a0c3acefe911fc472031cbfb48efade57525
Subhamay-Dey <ncdey1966@gmail.com>
2024-05-22 20:32:08 +0530

    Rotate List -- solved (leetcode) just a single case is incorrect when the k = size;

---
+++ LeetCode/problem42.java
@@ -0,0 +1,44 @@
+package LeetCode;
+
+public class problem42 {
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        
+        if(head == null || head.next == null || k == 0 ) return head;
+
+        int size = 1;
+        ListNode tail = head;
+        while(tail.next != null) {
+            size++;
+            tail = tail.next;
+        }
+
+        tail.next = head;
+
+        k = k % size;
+        if(size == k) return tail;
+
+        int index = size - k - 1;
+        int i = 0;
+        ListNode prev = head;
+        while(i < index) {
+            prev = prev.next;
+            i++;
+        }
+        ListNode newhead = prev.next;
+        prev.next = null;
+        tail.next = head;
+
+        return newhead;
+    }
+}
+}
