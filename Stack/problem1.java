2ec2492cc2cc7685829511133210630f2d02da37
Subhamay-Dey <ncdey1966@gmail.com>
2024-05-23 23:56:22 +0530

    successfully completed making a stack and also pushed elements into it and also printed it one by one and after peek we r poping the top element and like this the cycle goes untill the stack is empty.

--- Stack/problem1.java
+++ Stack/problem1.java
@@ -39,6 +39,16 @@ public class problem1 {
         }
     }
     public static void main(String args[]) {
+        Stack s = new Stack();
+        s.push(0);
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
 
+        while (!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
     }
 }
