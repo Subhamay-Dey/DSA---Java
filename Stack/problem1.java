4c8df71ef33966242d5ebf4b12a308c723b4883e
Subhamay-Dey <ncdey1966@gmail.com>
2024-05-23 23:52:01 +0530

    successfully implemented the push, peep amd pop functionality in stack.

--- Stack/problem1.java
+++ Stack/problem1.java
@@ -23,6 +23,20 @@ public class problem1 {
             newNode.next = head;
             head = newNode;
         }
+        public static int pop() {
+            if(isEmpty()) {
+                return -1;
+            }
+            int top = head.data;
+            head = head.next;
+            return top;
+        }
+        public static int peek() {
+            if(isEmpty()) {
+                return -1;
+            } 
+            return head.data;
+        }
     }
     public static void main(String args[]) {
 
