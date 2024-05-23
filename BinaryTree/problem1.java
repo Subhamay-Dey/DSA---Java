501ecdf9d794489f15c1199aa452b4ae85c05bcd
Subhamay-Dey <ncdey1966@gmail.com>
2024-05-24 02:01:43 +0530

    successfully completed my first binary tree program in which I have created a binary tree with left side dominant and then right node as usual using the given nodes in my void main function. :)

---
+++ BinaryTree/problem1.java
@@ -0,0 +1,39 @@
+package BinaryTree;
+
+public class problem1 {
+    public class BinaryTrees {
+        static class Node {
+            int data;
+            Node left;
+            Node right;
+
+            Node(int data) {
+                this.data = data;
+                this.left = null;
+                this.right = null;
+            }
+        }
+
+        static class BinaryBuildTree {
+            static int idx = -1;
+            public static Node buildTree(int nodes[]) {
+                idx++;
+                if(nodes[idx] == -1) {
+                    return null;
+                }
+                Node newNode = new Node(nodes[idx]);
+                newNode.left = buildTree(nodes);
+                newNode.right = buildTree(nodes);
+
+                return newNode;
+            }
+        }
+    
+    public static void main(String args[]) {
+        int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+        BinaryBuildTree tree = new BinaryBuildTree();
+        Node root = tree.buildTree(nodes);
+        System.out.println(root.data);
+    }
+    }
+}
