9154c7446258b20ab2d7ac03fd0f2f8ec139346f
Subhamay-Dey <ncdey1966@gmail.com>
2024-05-24 18:10:29 +0530

    Height of a tree -- also solved.

---
+++ BinaryTree/problem8.java
@@ -0,0 +1,51 @@
+package BinaryTree;
+
+public class problem8 {
+    public class binarytree {
+        static class Node {
+            int data;
+            Node left;
+            Node right;
+
+            Node(int data) {
+                this.data = data;
+                this.left = null;
+                this.right = null;
+            }
+        }
+
+        static class BinaryBuildTree {
+            static int idx = -1;
+            public static Node buildTree(int nodes[]) {
+                idx++;
+                if(nodes[idx] == -1) {
+                    return null;
+                }
+                Node newNode = new Node(nodes[idx]);
+                newNode.left = buildTree(nodes);
+                newNode.right = buildTree(nodes);
+
+                return newNode;
+            }
+        }
+
+        public static int height(Node root) {
+            if (root == null) {
+                return 0;
+            }
+            int leftHeight = height(root.left);
+            int rightheight = height(root.right);
+
+            int myHeight = Math.max(leftHeight, rightheight) + 1;
+
+            return myHeight;
+        }
+    
+    public static void main(String args[]) {
+        int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+        BinaryBuildTree tree = new BinaryBuildTree();
+        Node root = tree.buildTree(nodes);
+        System.out.println(height(root));
+    }
+    }
+}
