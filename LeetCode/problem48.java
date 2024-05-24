d3d70c6e187f57e32ae2b59715311278f65df8ef
Subhamay-Dey <ncdey1966@gmail.com>
2024-05-24 23:48:06 +0530

    94. Binary Tree Inorder Traversal -- solved in just 2 mins :))

---
+++ LeetCode/problem48.java
@@ -0,0 +1,33 @@
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class problem48 {
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    List<Integer> list = new ArrayList<>();
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if(root == null) return list;
+
+        inorderTraversal(root.left);
+        list.add(root.val);
+        inorderTraversal(root.right);
+
+        return list;
+    }
+}
+}
