ab1947565a5f7065a1d2b1bbb5740b1016ec501e
Subhamay-Dey <ncdey1966@gmail.com>
2024-06-15 03:53:00 +0530

    Minimum Number of Moves to Seat Everyone -- solved (leetcode).

---
+++ LeetCode/problem67.java
@@ -0,0 +1,18 @@
+package LeetCode;
+
+import java.util.Arrays;
+
+public class problem67 {
+    class Solution {
+        public int minMovesToSeat(int[] seats, int[] students) {
+            Arrays.sort(seats);
+            Arrays.sort(students);
+            int n=seats.length;
+            int sum=0;
+            for(int i=0;i<n;i++){
+                sum += Math.abs(seats[i]-students[i]);
+            }
+            return sum;
+        }
+    }
+}
