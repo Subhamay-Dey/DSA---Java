46a81b7e10a793fcdcca0fb4368aa79ba50c6895
Subhamay-Dey <ncdey1966@gmail.com>
2024-06-26 00:37:02 +0530

    Build Array from Permutation -- solved (leetcode).

---
+++ LeetCode/problem71.java
@@ -0,0 +1,13 @@
+package LeetCode;
+
+public class problem71 {
+    class Solution {
+        public int[] buildArray(int[] nums) {
+    
+        int[] ans = new int[nums.length];
+        for(int i = 0; i < nums.length; i++)
+        ans[i] = nums[nums[i]];
+        return ans;
+        }
+    }
+}
