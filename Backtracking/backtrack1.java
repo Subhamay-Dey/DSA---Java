ca245d1295e91005465e0c5449fbcd34bb15496c
Subhamay-Dey <ncdey1966@gmail.com>
2024-08-16 22:59:23 +0530

    Backtracking 1st program solved - Printing all the possible permutative outcome of a string.

---
+++ Backtracking/backtrack1.java
@@ -0,0 +1,23 @@
+package Backtracking;
+
+public class backtrack1 {
+
+    public static void printPermutation(String str, String perm, int index) {
+
+        if(str.length() == 0) {
+            System.out.println(perm);
+            return;
+        }
+
+        for(int i = 0; i < str.length(); i++ ) {
+            char currChar = str.charAt(i);
+            String newStr = str.substring(0, i) + str.substring(i + 1);
+            printPermutation(newStr, perm + currChar, 1);
+        }
+    }
+
+    public static void main(String args[]) {
+        String str = "ABC";
+        printPermutation(str, "", 0);
+    }
+}
