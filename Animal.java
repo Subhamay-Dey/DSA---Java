9cbf2cd5b818f04e0b712716ed89410fb7243d6b
Subhamay-Dey <ncdey1966@gmail.com>
2025-02-10 23:59:58 +0530

    . Use of super with Methods

---
+++ Animal.java
@@ -0,0 +1,23 @@
+public class Animal{
+    void eat(){
+     System.out.println("Eating food");
+     }
+    }
+    class Dog extends Animal{
+    void eat()
+    {
+     System.out.println("Eating bones ");
+    }
+    void display()
+    {
+     super.eat();
+     }
+    }
+    class Main{
+    public static void main(String args[]){
+    Dog d=new Dog();
+    d.display();
+     }
+    } {
+    
+}
