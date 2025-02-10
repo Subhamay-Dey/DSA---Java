326adb6ea016537912b2e0a416084bd25f7e89a7
Subhamay-Dey <ncdey1966@gmail.com>
2025-02-11 03:27:30 +0530

    Final keyword with variable done ✅

---
+++ Final/final_var.java
@@ -0,0 +1,11 @@
+package Final;
+
+public class final_var {
+    public static void main(String[] args) {
+        final int a = 10;
+        System.out.println(a);
+
+        //a = 20;
+        //System.out.println(a);
+    }
+}
