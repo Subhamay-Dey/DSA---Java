bec418f53906dace4cd79733a4cdbe104903ba6e
Subhamay Dey <ncdey1966@gmail.com>
2026-03-04 20:35:08 +0530

    Implement Unix path simplification using a stack

---
+++ Stack/problem3.java
@@ -0,0 +1,43 @@
+import java.util.Stack;
+
+
+public class UnixPath {
+
+    public static String InputPath(String path) {
+
+        Stack<String> stack = new Stack<>();
+        String[] parts = path.split("/");
+
+        for (int i = 0; i < parts.length; i++) {
+            String part = parts[i];
+
+            if (part.equals("") || part.equals(".")) {
+                continue;
+            }
+            else if (part.equals("..")) {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            }
+            else {
+                stack.push(part);
+            }
+        }
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < stack.size(); i++) {
+            String dir = stack.get(i);
+            result.append("/").append(dir);
+        }
+        return result.length() == 0 ? "/" : result.toString();
+
+    }
+
+    public static void main(String[] args) {
+
+        String input = "/home/../../tmp//./";
+        String output = InputPath(input);
+
+        System.out.println("Final Path: " + output);
+    }
+}
