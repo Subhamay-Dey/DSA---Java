569b8ee51437f80365e7e353dbb97fc935ce797c
Subhamay Dey <ncdey1966@gmail.com>
2026-03-05 05:45:52 +0530

    Added getMin and size method in S400 class and implemented all the methods in the main function and used stack to implement all the operation s seperately ✅

--- Stack/problem4.java
+++ Stack/problem4.java
@@ -4,7 +4,7 @@ public class S400 {
 
     ArrayList<Integer> list;
 
-    public S400 {
+    public S400() {
         list = new ArrayList<>();
     }
 
@@ -26,9 +26,54 @@ public class S400 {
         }
         return list.remove(list.size() - 1);
     }
+
+    public int getMin() {
+        if (isEmpty()) return -1;
+         int min = list.get(0);
+
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) < min) {
+                min = list.get(i);
+            }
+         }
+        return min;
+    }
+
+    public int size() {
+        return list.size();
+    }
 }
 public class main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        S400 stack = new S400();
+        stack.push(100);
+        stack.push(200);
+        stack.push(300);
+        System.out.println(stack.top());
+        System.out.println(stack.size());
+        System.out.println(stack.getMin());
+        System.out.println(stack.pop());
+        System.out.println(stack.top());
+        System.out.println(stack.size());
+        System.out.println(stack.isEmpty());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.isEmpty());
+        System.out.println(stack.size());
+
+
+        Stack<Integer> stack2 = new Stack<>();
+        stack2.push(100);
+        stack2.push(200);
+        stack2.push(300);
+        System.out.println(stack2.peek());
+        System.out.println(stack2.size());
+        System.out.println(stack2.pop());
+        System.out.println(stack2.peek());
+        System.out.println(stack2.isEmpty());
+        System.out.println(stack2.pop());
+        System.out.println(stack2.pop());
+        System.out.println(stack2.isEmpty());
+        System.out.println(stack2.size());
     }
 }
